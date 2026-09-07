60e8495f904844daf71572799719aad94474e770
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-09-07 18:50:42 +0500

    Refactor main.go for task completion functionality

--- Project#1/September-7-2026-CompletingATask/main.go
+++ Project#1/September-7-2026-CompletingATask/main.go
@@ -1,30 +1,87 @@
 package main
 
 import (
-        "fmt"
-        "bufio"
-        "os"
-        "strconv"
-        "strings"
-    )
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Task struct {
-    Name string
-    Completed bool
+	Name      string
+	Completed bool
 }
 
 func completedTask(tasklist *[]Task, index int) {
-    (*tasklist)[index].Completed = true
-} 
+	(*tasklist)[index].Completed = true
+}
+
+func viewAllTasks(tasklist []Task) {
+	for i := 0; i < len(tasklist); i++ {
+		if tasklist[i].Completed == true {
+			fmt.Printf("[x] %s\n", tasklist[i].Name)
+		} else {
+			fmt.Printf("[ ] %s\n", tasklist[i].Name)
+		}
+	}
+}
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
+	var tasklist []Task
 
-    for scanner.Scan() {
-        lines := scanner.Text()
-    }
+	scanner := bufio.NewScanner(os.Stdin)
 
-    
+	scanner.Scan()
+	taskCount, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return
+	}
 
+	scanner.Scan()
+	taskString := scanner.Text()
 
+	dividedString := strings.Split(taskString, ",")
+
+	for i := 0; i < taskCount; i++ {
+		currentTask := dividedString[i]
+		dividedTask := strings.Split(currentTask, ":")
+
+		completed, err := strconv.ParseBool(dividedTask[1])
+		if err != nil {
+			return
+		}
+
+		tasklist = append(tasklist, Task{
+			Name:      dividedTask[0],
+			Completed: completed,
+		})
+	}
+
+	scanner.Scan()
+	index, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return
+	}
+
+	completedTask(&tasklist, index)
+
+	viewAllTasks(tasklist)
+
+	fmt.Printf("Task '%s' marked as completed!\n", tasklist[index].Name)
+
+	completedCount := 0
+
+	for i := 0; i < len(tasklist); i++ {
+		if tasklist[i].Completed == true {
+			completedCount++
+		}
+	}
+
+	fmt.Printf(
+		"Total: %d tasks (%d completed, %d remaining)\n",
+		taskCount,
+		completedCount,
+		taskCount-completedCount,
+	)
 }
